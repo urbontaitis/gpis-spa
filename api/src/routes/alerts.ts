1703470118f3b734e56051304c07041a9cbbdbb1
Mindaugas Urbontaitis <mindaugas@urbontatis.lt>
2018-02-01 00:17:43 +0200

    simplified version of data scraping

--- api/src/routes/alerts.ts
+++ api/src/routes/alerts.ts
@@ -1,4 +1,6 @@
+import * as cheerio from "cheerio";
 import { Request, Response, Router } from "express";
+import * as fs from "fs";
 import * as request from "request-promise";
 import * as uuid from "uuid";
 
@@ -10,213 +12,53 @@ route.get("/", (req: Request, res: Response) => {
   if (lang === undefined) {
     res.json({});
   }
-
-  const responseResult = [];
   // http://gpis.vpgt.lt/go.php/eng/Sent-messages/301/
   // http://gpis.vpgt.lt/go.php/lit/Issiusti-pranesimai/300
   // http://gpis.vpgt.lt/go.php/rus/Otpravl-soobshtenija/275/
   request
     .get(`http://localhost:8000/${lang}.html`)
-    // tslint:disable-next-line:arrow-parens
-    .then(result => {
-      // tslint:disable-next-line:no-console
-      console.log("Started to parse");
-      const htmlParser = require("htmlparser2");
-      const handler = new htmlParser.DomHandler((error, dom) => {
-        if (error) {
-          // tslint:disable-next-line:no-console
-          console.error(`Klaida: ${error}`);
-        } else {
-          // tslint:disable-next-line:no-console
-          console.info(dom);
-          dom.forEach(html => {
-            if (html.name === "html") {
-              html.children.forEach(body => {
-                if (body.name === "body") {
-                  body.children.forEach(page => {
-                    if (
-                      page.name === "div" &&
-                      page.attribs !== undefined &&
-                      page.attribs.id === "page"
-                    ) {
-                      page.children.forEach(contents => {
-                        if (
-                          contents.name === "div" &&
-                          contents.attribs.id === "contents"
-                        ) {
-                          contents.children.forEach(middleSide => {
-                            if (
-                              middleSide.name === "div" &&
-                              middleSide.attribs.class === "middle_side"
-                            ) {
-                              middleSide.children.forEach(articleList => {
-                                if (
-                                  articleList.name === "div" &&
-                                  articleList.attribs.id === "v_article_list"
-                                ) {
-                                  articleList.children.forEach(
-                                    articleBlocks => {
-                                      if (
-                                        articleBlocks.name === "div" &&
-                                        articleBlocks.attribs.class ===
-                                          "article_block"
-                                      ) {
-                                        articleBlocks.children.forEach(
-                                          articleMainBlock => {
-                                            if (
-                                              articleMainBlock.name === "div" &&
-                                              articleMainBlock.attribs.class ===
-                                                "artivle_list_main_block"
-                                            ) {
-                                              let article = {
-                                                id: uuid.v4(),
-                                                title: "",
-                                                date: "",
-                                                description: "",
-                                                url: ""
-                                              };
-                                              articleMainBlock.children.forEach(
-                                                blocks => {
-                                                  if (
-                                                    blocks.name === "div" &&
-                                                    blocks.attribs.class ===
-                                                      "artivle_list_article_name"
-                                                  ) {
-                                                    blocks.children.forEach(
-                                                      name => {
-                                                        if (
-                                                          name.type ===
-                                                            "text" &&
-                                                          name.next === null
-                                                        ) {
-                                                          article.date =
-                                                            name.data;
-                                                        }
-                                                        if (
-                                                          name.name === "a" &&
-                                                          name.attribs.class ===
-                                                            "article-list"
-                                                        ) {
-                                                          article.title =
-                                                            name.children[0].data;
-                                                          article.url =
-                                                            name.attribs.href;
-                                                        }
-                                                      }
-                                                    );
-                                                  }
-                                                  if (
-                                                    blocks.name === "div" &&
-                                                    blocks.attribs.class ===
-                                                      "artivle_list_article_header"
-                                                  ) {
-                                                    let description = "";
-                                                    blocks.children.forEach(
-                                                      desc => {
-                                                        if (
-                                                          desc.children !==
-                                                          undefined
-                                                        ) {
-                                                          desc.children.forEach(
-                                                            descObj => {
-                                                              if (
-                                                                descObj.type !==
-                                                                "text"
-                                                              ) {
-                                                                if (
-                                                                  descObj
-                                                                    .children[0] !==
-                                                                    undefined &&
-                                                                  descObj
-                                                                    .children[0]
-                                                                    .type ===
-                                                                    "tag"
-                                                                ) {
-                                                                  descObj.children[0].children.forEach(
-                                                                    element => {
-                                                                      if (
-                                                                        element.type ===
-                                                                        "text"
-                                                                      ) {
-                                                                        description +=
-                                                                          element.data;
-                                                                      } else {
-                                                                        // tslint:disable-next-line:no-console
-                                                                        console.log(
-                                                                          element
-                                                                        );
-                                                                      }
-                                                                    }
-                                                                  );
-                                                                }
-                                                                if (
-                                                                  descObj
-                                                                    .children[0] !==
-                                                                    undefined &&
-                                                                  descObj
-                                                                    .children[0]
-                                                                    .type ===
-                                                                    "text"
-                                                                ) {
-                                                                  description +=
-                                                                    descObj
-                                                                      .children[0]
-                                                                      .data;
-                                                                }
-                                                              }
-                                                              // jei neturi spano
-                                                              if (
-                                                                descObj.type ===
-                                                                "text"
-                                                              ) {
-                                                                description +=
-                                                                  descObj.data;
-                                                              }
-                                                            }
-                                                          );
-                                                        } else {
-                                                          // tslint:disable-next-line:no-console
-                                                          // console.error(desc);
-                                                        }
-                                                      }
-                                                    );
-                                                    article.description = description;
-                                                  }
-                                                }
-                                              );
-                                              responseResult.push(article);
-                                            }
-                                          }
-                                        );
-                                      }
-                                    }
-                                  );
-                                }
-                              });
-                            }
-                          });
-                        }
-                      });
-                    }
-                  });
-                }
-              });
-            }
-          });
-        }
+    .then((result: any) => {
+      const $ = cheerio.load(result);
+      const alertsToList = [];
+      $(".artivle_list_main_block").each((i, elem) => {
+        const articleNameBlock = $(elem)
+          .find(".artivle_list_article_name")
+          .text()
+          .trim()
+          .split(" ");
+        alertsToList[i] = {
+          id: uuid.v4(),
+          title: $(elem)
+            .find(".artivle_list_article_name")
+            .children(".article-list")
+            .text(),
+          date: articleNameBlock[articleNameBlock.length - 1],
+          description: $(elem)
+            .find("p")
+            .text()
+            .trim(),
+          url: $(elem)
+            .find(".article_header")
+            .attr("href")
+        };
       });
 
-      const parser = new htmlParser.Parser(handler, {
-        decodeEntities: true,
-        normalizeWhitespace: true
-      });
-      // parser.parseChunk(result);
-      parser.write(result);
-      parser.end();
-      // tslint:disable-next-line:no-console
-      console.log("Ended to parse");
-      res.send({ alerts: responseResult });
+      res.send({ alerts: alertsToList });
+    })
+    .catch((err: any) => {
+      res.json({ errors: err });
     });
+});
+
+route.post("/sync", (req: Request, res: Response) => {
+  // example how to store to json :)
+  const alertsToList = [];
+  const alertsToListTrimmed = alertsToList.filter((n: any) => n !== undefined);
+  fs.writeFile(
+    "alerts.json",
+    JSON.stringify(alertsToListTrimmed, null, 4),
+    (err: any) => res.send("File successfully written!")
+  );
 });
 
 export default route;
